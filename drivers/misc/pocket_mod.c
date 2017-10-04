varunchitre15/thunderzap_sprout

pocket_mod: squash all the enhancements
pocket_mod: add timeout option

do not waste ~50ms with every touch. bail out quicker if two
presses are in 'immediate' succession (<k ms) where the proximity
value need not be necessarily read again (in real world scenarios).

sysfs/pocket_mod/pocket_mod_timeout: k ms (0: disable)

Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

pocket_mod: rename pocket_mod_timeout -> timeout

Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

pocket_mod: pocket_mod_enable -> enable

just name this directly, we have no obstacles here

Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

pocket_mod: sysfs: enable: sysfs can't be world writable

Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

pocket_mod: bugfix: return previous result as intended

Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

pocket_mod: stop spamming kmsg

pocket_mod: add sensor_check ptf to int fn(void)

move pocket_mod_init to late_initcall

Signed-off-by: thewisenerd <thewisenerd@protonmail.com>

pocket_mod: set timeout to 600ms as default value

Signed-off-by: Levin Calado <levincalado@gmail.com>

pocket_mod: cm36283: remove unused als value

fetching als value here is useless

Signed-off-by: Levin Calado <levincalado@gmail.com>

pocket_mod: tmd2771: remove unused als value

fetching als value here is useless

Signed-off-by: Levin Calado <levincalado@gmail.com>

drivers/misc/pocket_mod.c
@@ -33,7 +33,7 @@
 #include <linux/pocket_mod.h>
 
 int is_screen_on;
-char alsps_dev;
+char alsps_dev = 0;
 
 #ifdef CONFIG_POCKETMOD
 unsigned pocket_mod_switch = 1;
@@ -41,31 +41,38 @@ unsigned pocket_mod_switch = 1;
 unsigned pocket_mod_switch = 0;
 #endif
 
+static unsigned int pocket_mod_timeout = 600;
+static cputime64_t read_time_pre = 0;
+static int prev_res = 0;
+
+static int (*sensor_check)(void) = NULL;
+
 int device_is_pocketed(void) {
 
 	if (!(pocket_mod_switch))
 		return 0;
 
+	if (sensor_check == NULL)
+		return 0;
+
 	if (!(is_screen_on)) {
-		if (pocket_mod_switch){
-			if (alsps_dev == 't')
-			{
-				if (tmd2771_pocket_detection_check() == 1)
-					return 0;
-				else
-					return 1;
+		if (pocket_mod_timeout) {
+			if ((ktime_to_ms(ktime_get()) - read_time_pre) < pocket_mod_timeout) {
+				return prev_res;
 			}
-			else if (alsps_dev == 'c')
-			{
-				if (cm36283_pocket_detection_check() == 1)
-					return 0;
-				else
-					return 1;
+			read_time_pre = ktime_to_ms(ktime_get());
+		}
+		if (pocket_mod_switch){
+			if (sensor_check() == 1) {
+				prev_res = 0;
+				return 0;
+			} else {
+				prev_res = 1;
+				return 1;
 			}
 		}
 	}
 
-	printk(KERN_INFO "%s: screen is on\n", __func__);
 	return 0;
 }
 
@@ -88,12 +95,34 @@ static ssize_t pocket_mod_set(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(pocket_mod_enable, 0777,
+static DEVICE_ATTR(enable, (S_IWUSR|S_IRUGO),
 		pocket_mod_show, pocket_mod_set);
+
+static ssize_t pocket_mod_timeout_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", pocket_mod_timeout);
+}
+
+static ssize_t pocket_mod_timeout_set(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	unsigned int val = 0;
+
+	if (sscanf(buf, "%u\n", &val) == 1) {
+		pocket_mod_timeout = val;
+	}
+
+	return size;
+}
+
+static DEVICE_ATTR(timeout, (S_IWUSR|S_IRUGO),
+		pocket_mod_timeout_show, pocket_mod_timeout_set);
 
 static struct attribute *pocket_mod_attributes[] =
 {
-	&dev_attr_pocket_mod_enable.attr,
+	&dev_attr_enable.attr,
+	&dev_attr_timeout.attr,
 	NULL
 };
 
@@ -117,8 +146,6 @@ static int pocket_mod_init_sysfs(void) {
 	struct kobject *pocket_mod_kobj;
 	pocket_mod_kobj = kobject_create_and_add("pocket_mod", NULL);
 
-	dev_attr_pocket_mod_enable.attr.name = "enable";
-
 	rc = sysfs_create_group(pocket_mod_kobj,
 			&pocket_mod_group);
 
@@ -129,4 +156,20 @@ static int pocket_mod_init_sysfs(void) {
 
 }
 
-module_init(pocket_mod_init_sysfs);
+static int pocket_mod_init(void) {
+
+	int rc = 0;
+
+	rc = pocket_mod_init_sysfs();
+
+	if (alsps_dev == 't') {
+		sensor_check = tmd2771_pocket_detection_check;
+	} else if (alsps_dev == 'c') {
+		sensor_check = cm36283_pocket_detection_check;
+	}
+
+	return rc;
+
+}
+
+late_initcall(pocket_mod_init);

manjotsidhu/wakemod

drivers/misc/pocket_mod.c
@@ -33,7 +33,7 @@
 #include <linux/pocket_mod.h>
 
 int is_screen_on;
-char alsps_dev;
+char alsps_dev = 0;
 
 #ifdef CONFIG_POCKETMOD
 unsigned pocket_mod_switch = 1;
@@ -41,24 +41,39 @@ unsigned pocket_mod_switch = 1;
 unsigned pocket_mod_switch = 0;
 #endif
 
+static unsigned int pocket_mod_timeout = 600;
+static cputime64_t read_time_pre = 0;
+static int prev_res = 0;
+
+static int (*sensor_check)(void) = NULL;
+
+
 int device_is_pocketed(void) {
 
 	if (!(pocket_mod_switch))
 		return 0;
 
+	if (sensor_check == NULL)
+		return 0;
+
 	if (!(is_screen_on)) {
+		if (pocket_mod_timeout) {
+			if ((ktime_to_ms(ktime_get()) - read_time_pre) < pocket_mod_timeout) {
+				return prev_res;
+			}
+			read_time_pre = ktime_to_ms(ktime_get());
+		}
 		if (pocket_mod_switch){
-			if (alsps_dev == 'c')
-			{
-				if (ap3220_pocket_detection_check() == 1)
-					return 0;
-				else
-					return 1;
+			if (sensor_check() == 1) {
+				prev_res = 0;
+				return 0;
+			} else {
+				prev_res = 1;
+				return 1;
 			}
 		}
 	}
 
-	printk(KERN_INFO "%s: screen is on\n", __func__);
 	return 0;
 }
 
@@ -81,12 +96,34 @@ static ssize_t pocket_mod_set(struct device *dev,
 	return size;
 }
 
-static DEVICE_ATTR(pocket_mod_enable, 0777,
+static DEVICE_ATTR(enable, (S_IWUSR|S_IRUGO),
 		pocket_mod_show, pocket_mod_set);
+
+static ssize_t pocket_mod_timeout_show(struct device *dev,
+				struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%u\n", pocket_mod_timeout);
+}
+
+static ssize_t pocket_mod_timeout_set(struct device *dev,
+		struct device_attribute *attr, const char *buf, size_t size)
+{
+	unsigned int val = 0;
+
+	if (sscanf(buf, "%u\n", &val) == 1) {
+		pocket_mod_timeout = val;
+	}
+
+	return size;
+}
+
+static DEVICE_ATTR(timeout, (S_IWUSR|S_IRUGO),
+		pocket_mod_timeout_show, pocket_mod_timeout_set);
 
 static struct attribute *pocket_mod_attributes[] =
 {
-	&dev_attr_pocket_mod_enable.attr,
+	&dev_attr_enable.attr,
+	&dev_attr_timeout.attr,
 	NULL
 };
 
@@ -110,8 +147,6 @@ static int pocket_mod_init_sysfs(void) {
 	struct kobject *pocket_mod_kobj;
 	pocket_mod_kobj = kobject_create_and_add("pocket_mod", NULL);
 
-	dev_attr_pocket_mod_enable.attr.name = "enable";
-
 	rc = sysfs_create_group(pocket_mod_kobj,
 			&pocket_mod_group);
 
@@ -122,4 +157,18 @@ static int pocket_mod_init_sysfs(void) {
 
 }
 
-module_init(pocket_mod_init_sysfs);
+static int pocket_mod_init(void) {
+
+	int rc = 0;
+
+	rc = pocket_mod_init_sysfs();
+
+	if (alsps_dev == 'c') {
+		sensor_check = ap3220_pocket_detection_check;
+	}
+
+	return rc;
+
+}
+
+late_initcall(pocket_mod_init);