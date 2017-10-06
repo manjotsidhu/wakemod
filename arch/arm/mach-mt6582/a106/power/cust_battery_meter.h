afa9e4238e468af506ad51b048986029735ea0d1
Manjot Sidhu <manjot.gni@gmail.com>
2017-10-06 20:41:56 +0530

    a106: power: Improved Battery Life and Backup

--- arch/arm/mach-mt6582/a106/power/cust_battery_meter.h
+++ arch/arm/mach-mt6582/a106/power/cust_battery_meter.h
@@ -35,15 +35,15 @@
 #define FG_METER_RESISTANCE 	0
 
 /* Qmax for battery  */
-#define Q_MAX_POS_50	1399
-#define Q_MAX_POS_25	1499
-#define Q_MAX_POS_0		1377
-#define Q_MAX_NEG_10	1237
+#define Q_MAX_POS_50	1865
+#define Q_MAX_POS_25	1841
+#define Q_MAX_POS_0		1624
+#define Q_MAX_NEG_10	1541
 
-#define Q_MAX_POS_50_H_CURRENT	1377
-#define Q_MAX_POS_25_H_CURRENT	1473
-#define Q_MAX_POS_0_H_CURRENT	1247
-#define Q_MAX_NEG_10_H_CURRENT	819
+#define Q_MAX_POS_50_H_CURRENT	1797
+#define Q_MAX_POS_25_H_CURRENT	1826
+#define Q_MAX_POS_0_H_CURRENT	1178
+#define Q_MAX_NEG_10_H_CURRENT	1117
 
 
 /* Discharge Percentage */
@@ -52,16 +52,10 @@
 
 /* battery meter parameter */
 #define CUST_TRACKING_POINT  14
-#define CUST_HW_CC 		    0
+#define CUST_R_SENSE         68
+#define CUST_HW_CC           0
 #define AGING_TUNING_VALUE   103
 #define CUST_R_FG_OFFSET    0
-#ifdef CONFIG_MTK_FAN5405_SUPPORT
-#define CUST_R_SENSE         82
-#elif defined(CONFIG_MTK_BQ24158_SUPPORT)
-#define CUST_R_SENSE         68
-#else
-#define CUST_R_SENSE         200
-#endif
 
 #define OCV_BOARD_COMPESATE	0 //mV 
 #define R_FG_BOARD_BASE		1000
