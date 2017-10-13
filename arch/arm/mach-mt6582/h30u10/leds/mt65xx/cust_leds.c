8a2287c84a2459ba67bf4472b5a6d3c6ef4f8cc3
imranpopz <imranpopz32@gmail.com>
2017-10-13 20:27:58 +0530

    Revert "H30u10 : Blackscreen fixed"
    This reverts commit a6e5aa17ab3eed7c9d0b42a5f4a2884ad8a6e48a.

--- arch/arm/mach-mt6582/h30u10/leds/mt65xx/cust_leds.c
+++ arch/arm/mach-mt6582/h30u10/leds/mt65xx/cust_leds.c
@@ -12,7 +12,6 @@
 
 extern int disp_bls_set_backlight(unsigned int level);
 
-extern int mtkfb_set_backlight_level(unsigned int level);
 // Only support 64 levels of backlight (when lcd-backlight = MT65XX_LED_MODE_PWM)
 #define BACKLIGHT_LEVEL_PWM_64_FIFO_MODE_SUPPORT 64 
 // Support 256 levels of backlight (when lcd-backlight = MT65XX_LED_MODE_PWM)
@@ -33,13 +32,6 @@ unsigned int brightness_mapping(unsigned int level)
     mapped_level = level;
        
 	return mapped_level;
-}
-//use LCD IC control backlight
-unsigned int Cust_SetBacklight(int level, int div)
-{
-    printk("Cust_SetBacklight level=%d\n", level);
-    mtkfb_set_backlight_level(level);
-    return 0;
 }
 /*
 unsigned int Cust_SetBacklight(int level, int div)
@@ -122,12 +114,11 @@ unsigned int Cust_SetBacklight(int level, int div)
 static struct cust_mt65xx_led cust_led_list[MT65XX_LED_TYPE_TOTAL] = {
 	{"red",               MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK1,{0}},
 	{"green",             MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK0,{0}},
-	{"blue",              MT65XX_LED_MODE_NONE, -1,{0}},
+	{"blue",              MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK2,{0}},
 	{"jogball-backlight", MT65XX_LED_MODE_NONE, -1,{0}},
 	{"keyboard-backlight",MT65XX_LED_MODE_NONE, -1,{0}},
 	{"button-backlight",  MT65XX_LED_MODE_PMIC, MT65XX_LED_PMIC_NLED_ISINK3,{0}},
-	//{"lcd-backlight",     MT65XX_LED_MODE_CUST_BLS_PWM, (int)disp_bls_set_backlight,{0}},
-	{"lcd-backlight",     MT65XX_LED_MODE_CUST_LCM, (int)Cust_SetBacklight,{0}},
+	{"lcd-backlight",     MT65XX_LED_MODE_CUST_BLS_PWM, (int)disp_bls_set_backlight,{0}},
 };
 
 struct cust_mt65xx_led *get_cust_led_list(void)
